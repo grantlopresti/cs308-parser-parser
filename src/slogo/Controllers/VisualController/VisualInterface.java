d389c3c984ccc156d643c871371d4a352fe22d08
Alex Xu <alex.xu@duke.edu>
2020-02-18 22:49:28 -0500

    Clarified ModelInterface (API), ModelObject (Abstract), ModelTurtle

---
+++ src/slogo/Controllers/VisualController/VisualInterface.java
@@ -0,0 +1,4 @@
+package slogo.Controllers.VisualController;
+
+public interface VisualInterface {
+}
--- src/slogo/exceptions/InvalidXMLConfigException.java
+++ src/slogo/exceptions/InvalidXMLConfigException.java
@@ -2,7 +2,7 @@ package slogo.exceptions;
 
 /**
  * Exception is thrown when there is an Invalid XML Configuration file, for configuring aesthetics.
- * @author Alex Xu, aqx
+ * @author Alex Xu
  */
 public class InvalidXMLConfigException extends RuntimeException {
 }
