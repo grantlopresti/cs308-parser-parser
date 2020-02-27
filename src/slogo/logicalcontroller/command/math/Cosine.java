bb43e05a41077d9b96ff6f45f2acf5093ca8c755
Max Smith <ms724@duke.edu>
2020-02-26 23:13:03 -0500

    changes to cosine

--- src/slogo/logicalcontroller/command/math/Cosine.java
+++ src/slogo/logicalcontroller/command/math/Cosine.java
@@ -3,14 +3,6 @@ package slogo.logicalcontroller.command.math;
 import slogo.logicalcontroller.command.Command;
 
 public class Cosine extends MathCommand {
-<<<<<<< HEAD
-=======
-    private double value;
-
-    public Cosine(String inputvalue){
-        super(inputvalue);
-        value = Double.parseDouble(inputvalue);
->>>>>>> master
 
     public Cosine(String input){
         super(input);
