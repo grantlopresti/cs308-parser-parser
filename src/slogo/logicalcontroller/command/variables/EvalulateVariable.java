b250c864b52f1222f3548940889013094c53883b
amjad.syedibrahim <amjad.syedibrahim@duke.edu>
2020-03-05 16:48:36 -0500

    Filled out Evaluate Variable class to replace the variable name with a value

--- src/slogo/logicalcontroller/command/variables/EvalulateVariable.java
+++ src/slogo/logicalcontroller/command/variables/EvalulateVariable.java
@@ -1,8 +1,26 @@
 package slogo.logicalcontroller.command.variables;
 
+import slogo.logicalcontroller.variable.Variable;
+import slogo.logicalcontroller.variable.VariableList;
+
 /**
  * Command to evaluate variables
  * @author Alex Xu
  */
 public class EvalulateVariable {
+
+    double varToBeRet;
+
+    public EvalulateVariable(String name, VariableList variableList){
+
+        for(Object v: variableList){
+            if(variableList.isSameVariable(name, (Variable) v)){
+                varToBeRet = ((Variable) v).getValue();
+            }
+        }
+    }
+
+    public double getReturnValue(){
+        return this.varToBeRet;
+    }
 }
