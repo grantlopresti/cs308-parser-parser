520e514e668b15fbf71625d4dfada1e1314bff57
Alex Xu <alex.xu@duke.edu>
2020-03-05 23:50:31 -0500

    Fixed error thrown by QuerieCommand to be ResourceBundleException instead of parameterException

--- src/slogo/logicalcontroller/codefilters/MasterCodeFilter.java
+++ src/slogo/logicalcontroller/codefilters/MasterCodeFilter.java
@@ -1,14 +1,34 @@
 package slogo.logicalcontroller.codefilters;
 
+import slogo.exceptions.ResourceBundleCreationException;
+import slogo.logicalcontroller.BundleInterface;
+
+import java.util.ResourceBundle;
+
 /**
  * Utility class that filters the user input before sending it to the parser.
  * final keyword prevents subclassing and improves efficiency.
  * @author Alex Xu
  */
 public final class MasterCodeFilter {
-    private MasterCodeFilter(){};
+
+    private static final String RESOURCE_BUNDLE_LOCATION = "src/properties/inputFilters.properties";
+
+    private MasterCodeFilter(){
+        throw new AssertionError("Instantiating utility class.");
+    }
 
     public static String filterComments(String rawInput){
+        String result;
+
+        ResourceBundle filtersApplied;
+        try {
+            filtersApplied = BundleInterface.createResourceBundle(RESOURCE_BUNDLE_LOCATION);
+        } catch (Exception e) {
+            throw new ResourceBundleCreationException();
+        }
+
+
 
     }
 }
