531624ecce8bbb549d795fa11a98caa00befb69f
Alex Xu <alex.xu@duke.edu>
2020-03-06 11:27:14 -0500

    Updated CodeFilters

--- src/slogo/logicalcontroller/codefilters/MasterCodeFilter.java
+++ src/slogo/logicalcontroller/codefilters/MasterCodeFilter.java
@@ -3,7 +3,10 @@ package slogo.logicalcontroller.codefilters;
 import slogo.exceptions.ResourceBundleCreationException;
 import slogo.logicalcontroller.BundleInterface;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 /**
  * Utility class that filters the user input before sending it to the parser.
@@ -14,13 +17,15 @@ public final class MasterCodeFilter {
 
     private static final String RESOURCE_BUNDLE_LOCATION = "src/properties/inputFilters.properties";
 
+    /**
+     * Applies all of the filters, user-defined through a properties file.
+     */
     private MasterCodeFilter(){
         throw new AssertionError("Instantiating utility class.");
     }
 
     public static String filterComments(String rawInput){
         String result;
-
         ResourceBundle filtersApplied;
         try {
             filtersApplied = BundleInterface.createResourceBundle(RESOURCE_BUNDLE_LOCATION);
@@ -28,7 +33,21 @@ public final class MasterCodeFilter {
             throw new ResourceBundleCreationException();
         }
 
+        List<String> activeFilters = extractActiveFilters(filtersApplied);
 
+    }
 
+    private static List<String> extractActiveFilters(ResourceBundle filterInformation){
+        List<String> activeFiltersList = new ArrayList<>();
+
+        Set<String> keys = filterInformation.keySet();
+        for(String key : keys){
+            String value = filterInformation.getString(key);
+            boolean activeState = Boolean.parseBoolean(value);
+            if(activeState){
+                activeFiltersList.add(key);
+            }
+        }
+        return activeFiltersList;
     }
 }
