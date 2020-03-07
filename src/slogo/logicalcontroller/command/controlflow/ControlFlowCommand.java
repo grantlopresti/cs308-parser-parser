9eee272ad2e055a6d3528ef376bf041cfee01911
Alex Xu <alex.xu@duke.edu>
2020-03-07 00:18:25 -0500

    Refactored controlflow execute() methods to be in the abstract class instead

--- src/slogo/logicalcontroller/command/controlflow/ControlFlowCommand.java
+++ src/slogo/logicalcontroller/command/controlflow/ControlFlowCommand.java
@@ -1,6 +1,7 @@
 package slogo.logicalcontroller.command.controlflow;
 
 import slogo.logicalcontroller.command.Command;
+import slogo.model.ModelTurtle;
 
 import java.util.List;
 
@@ -37,5 +38,10 @@ public abstract class ControlFlowCommand implements Command {
 
     protected void setUnraveledCode(List<String> input){
         unraveledCode = input;
+    }
+
+    @Override
+    public String execute(ModelTurtle turtle) {
+        return null;
     }
 }
