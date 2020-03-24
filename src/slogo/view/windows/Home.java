0f540c80a448155c91fe2893a16dd9be08ac2c3e
Grant LoPresti <gjl13@duke.edu>
2020-03-24 15:56:52 -0400

    Complete JavaDoc Comments for Home.java

--- src/slogo/view/windows/Home.java
+++ src/slogo/view/windows/Home.java
@@ -33,8 +33,16 @@ public class Home extends Application {
 
   /**
    * Entry point for application
+   *
+   * This creates the Splash screen using FXML, I am aware that this is not the ideal way of
+   * doing it, but it was taken from my previous project and as I felt I demonstrated my ability
+   * to properly use JavaFX for the main Slogo frame, I tried to save time here so other issues
+   * could be addressed.
+   *
    * @param primaryStage the main stage for the Home view
    * @throws IOException caused by FileInputStream for loading the FXML
+   *
+   * @author Grant LoPresti
    */
   @Override
   public void start(Stage primaryStage) throws IOException {
@@ -52,17 +60,19 @@ public class Home extends Application {
 
   /**
    * Sets actions for buttons
-   *  Both for launching the application
-   *  and for changing settings
+   *  Both for launching the application and for changing settings
+   *
+   *  Methods were not able to be completed to implement the workspace settings buttons
+   *  This could easily be done if we had designed/developed an API for doing so.
+   *  As we were behind on much of the rest of the project, we did not reach the implementation
+   *  of these more advanced features, but these buttons would have prompted pop-up windows that
+   *  allowed the user to choose their settings and options for the workspace they were about to
+   *  create
+   *
+   *  @author Grant LoPresti
    */
   public void initialize() {
-    LaunchSlogo.setOnAction(e -> {
-      try {
-        launchSim();
-      } catch (IOException ex) {
-        System.out.println("INVALID SIMULATION LAUNCH ERROR");
-      }
-    });
+    LaunchSlogo.setOnAction(e -> launchSim());
     //TODO: Add actual events to these button clicks
     ColorScheme.setOnAction(e -> System.out.println("ColorScheme"));
     Language.setOnAction(e -> System.out.println("Language Select"));
@@ -70,11 +80,24 @@ public class Home extends Application {
     Help.setOnAction(e -> System.out.println("Display Help Menu"));
   }
 
+  /**
+   * Lanuches the simulation by creating a new manager class with the default settings (these
+   * setting currently include just the language, but would have been expanded to include the
+   * remaining settings as well had there been more time/less other issues)
+   *
+   * @author Grant LoPresti
+   */
   @FXML
-  private void launchSim() throws IOException {
+  private void launchSim() {
     new Manager(myDefaultLang);
   }
 
+  /**
+   * Takes in a new language and sets this as the initial language for the workspace to have when
+   * it is created. Later saved as a property of the workspace.
+   * @param newLang is a string of the new language ex: "English"
+   * @author Grant LoPresti
+   */
   private void setDefaultLang(String newLang) {
     myDefaultLang = newLang;
   }
